0ba83b477e512a3e0dad627830bbf35d562d3f0a
余铁铮 <784250886@qq.com>
2019-03-25 14:03:57 +0800

    config camera

---
+++ include/config.h
@@ -0,0 +1,36 @@
+//
+//  Created by Tyson YU on 3/25/2019
+//
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "common_include.h" 
+
+namespace VO 
+{
+
+class Config
+{
+private:
+    static std::shared_ptr<Config> config_; 
+    cv::FileStorage file_;
+    
+    Config () {} // private constructor makes a singleton
+public:
+    ~Config();  // close the file when deconstructing 
+    
+    // set a new config file 
+    static void setParameterFile( const std::string& filename ); 
+    
+    // access the parameter values
+    template< typename T >
+    static T get( const std::string& key )
+    {
+        return T( Config::config_->file_[key] );
+    }
+};
+
+}
+
+#endif // CONFIG_H
--- main.cpp
+++ main.cpp
@@ -2,35 +2,11 @@
 // create by Tiezheng YU on 3/14/2019
 //
 
-#include <LoadData.h>
+#include <common_include.h>
 
 
 int main(int argc, char **argv)
 {
-    //--------- 测试集第263个图片 ---------------训练集第2051个图片----------
-    pcl::visualization::PCLVisualizer viewer("result");
-    PNPSolver solver;
-    // solver.KeyPoints.ShowKeyPointCloud();
-    for(int i = 120; i < 1530; i = i+10)
-    {
-        std::cout << " test image number = " << i << std::endl;
-        cv::waitKey(0);
-        clock_t start = clock();
-        solver.FindMatches(i);
-        clock_t end = clock();
-        std::cout << "=========================================================================" << std::endl;
-        std::cout << "one frame use time = " << (double)(end-start)/CLOCKS_PER_SEC <<  " s"<< std::endl;
-        // solver.SolvePNP();
-        solver.ShowResults();
-        std::cout << "starting show results ..." << std::endl;
-        viewer.addPointCloud(solver.result_cloud.makeShared(),solver.TestImageName.timestamps.at(i));
-        viewer.setBackgroundColor(0,0,0);
-        viewer.addCoordinateSystem();
-        viewer.spin();
-        viewer.removeCoordinateSystem();
-        viewer.removeAllPointClouds(); 
-    }
-    
-
+   
     return 0;
 }
